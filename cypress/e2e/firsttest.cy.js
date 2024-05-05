8433641935750e2773fdf322aed50985369bbfb9
ABS RAKIB <abu35-1994@diu.edu.bd>
2024-05-05 11:39:48 +0600

    Github Action

--- cypress/e2e/firsttest.cy.js
+++ cypress/e2e/firsttest.cy.js
@@ -1,4 +1,11 @@
 describe('My First Test', () => {
+  before(() => {
+    cy.fixture('example').then((data) => {
+      console.log(data)
+    })
+  })
+
+
   it('Visits the Kitchen Sink', () => {
     cy.visit('https://example.cypress.io')
     cy.contains('prev').click()
